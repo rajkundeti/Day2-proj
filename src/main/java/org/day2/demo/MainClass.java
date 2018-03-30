fcdf341ef09b4c6a48d2fd1a5e937631e191fbba
rkundeti <rkundeti@DIN80011662.corp.capgemini.com>
2018-03-30 11:05:57 +0530

    initial changes

---
+++ src/main/java/org/day2/demo/MainClass.java
@@ -0,0 +1,17 @@
+package org.day2.demo;
+
+public class MainClass {
+	public static void main(String[] args) {
+		greetings();
+		
+		SalesClass obj=new SalesClass();
+		obj.show();
+
+	}
+	
+	public static void greetings(){
+		System.out.println("Hello! Good Morning!");
+	}
+
+
+}
---
+++ src/main/java/org/day2/demo/SalesClass.java
@@ -0,0 +1,10 @@
+package org.day2.demo;
+
+public class SalesClass {
+	public void show(){
+		int[] arg={12,45,67,90};
+		
+		System.out.println("Show Method");
+	}
+
+}
